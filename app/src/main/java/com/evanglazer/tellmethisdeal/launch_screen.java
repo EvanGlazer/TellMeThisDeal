1fd73c2028878b30ef7017ecc9cb49e9c6063321
unknown <evanowner@live.com>
2015-12-20 19:56:10 -0500

    added menu

--- app/src/main/java/com/evanglazer/tellmethisdeal/launch_screen.java
+++ app/src/main/java/com/evanglazer/tellmethisdeal/launch_screen.java
@@ -1,13 +1,28 @@
 package com.evanglazer.tellmethisdeal;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class launch_screen extends AppCompatActivity {
+public class launch_screen extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
     }
+    /**
+     * Initializes the activity menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }
+
